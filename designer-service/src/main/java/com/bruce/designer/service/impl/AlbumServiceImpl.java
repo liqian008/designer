64212925c3c79ee99470ef9a09f535eb2b39c423
liqian <liqian@aca80177.ipt.aol.com>
2013-07-03 01:21:15 +0800

    commit

--- designer-service/src/main/java/com/bruce/designer/service/impl/AlbumServiceImpl.java
+++ designer-service/src/main/java/com/bruce/designer/service/impl/AlbumServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.bruce.designer.bean.TbAlbum;
 import com.bruce.designer.bean.TbAlbumCriteria;
-<<<<<<< HEAD
 import com.bruce.designer.constant.ConstService;
 import com.bruce.designer.dao.TbAlbumMapper;
 import com.bruce.designer.service.AlbumService;
@@ -49,41 +48,6 @@ public class AlbumServiceImpl implements AlbumService {
 		TbAlbumCriteria criteria = new TbAlbumCriteria();
 		criteria.createCriteria().andUserIdEqualTo(userId).andStatusEqualTo(ConstService.ALBUM_OPEN_STATUS);
 		criteria.setOrderByClause("id desc");
-=======
-import com.bruce.designer.dao.TbAlbumMapper;
-import com.bruce.designer.service.AlbumService;
-
-@Service
-public class AlbumServiceImpl implements AlbumService {
-
-	@Autowired
-	private TbAlbumMapper albumMapper;
-
-	public int save(TbAlbum t) {
-		return albumMapper.insert(t);
-	}
-
-	public List<TbAlbum> queryAll() {
-		return albumMapper.selectByExample(null);
-	}
-
-	public int updateById(TbAlbum t) {
-		return albumMapper.updateByPrimaryKeySelective(t);
-	}
-
-	public int deleteById(Integer id) {
-		return albumMapper.deleteByPrimaryKey(id);
-	}
-
-	public TbAlbum loadById(Integer id) {
-		return albumMapper.selectByPrimaryKey(id);
-	}
-
-	public List<TbAlbum> queryAlbumByStatus(short status) {
-		TbAlbumCriteria criteria = new TbAlbumCriteria();
-		criteria.setOrderByClause("create_time desc");
-		criteria.createCriteria().andStatusEqualTo(status);
->>>>>>> branch 'master' of https://github.com/liqian008/designer.git
 		return albumMapper.selectByExample(criteria);
 	}
 
