71e9b897fee6309ed936d899988558b4f6d34764
liqian <liqian@127.0.0.1>
2013-08-28 22:49:43 +0800

    微博登陆

--- designer-front/src/main/java/com/bruce/designer/front/constants/ConstFront.java
+++ designer-front/src/main/java/com/bruce/designer/front/constants/ConstFront.java
@@ -5,6 +5,8 @@ public interface ConstFront {
 	/* for login session attribute*/
 	public static final String CURRENT_USER = "_currentUser";
 	
+	public static final String TEMPLATE_ACCESS_TOKEN = "_templateAccessToken";
+	
 	public static final String YYYY_MM_DD_FORMAT = "yyyy_MM_dd"; 
 	
 	public static final String YYYY_MM_DD_HH_MM_FORMAT = "yyyy-MM-dd HH:mm";
