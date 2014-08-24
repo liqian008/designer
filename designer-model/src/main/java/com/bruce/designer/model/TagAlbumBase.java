b16dca8b34f5aba9c0b2537f4cbdd97fcafb8c79
liqian <liqian008@sina.com>
2014-08-24 14:31:43 +0800

    使用mybatis分页插件重新生成mapper

---
+++ designer-model/src/main/java/com/bruce/designer/model/TagAlbumBase.java
@@ -0,0 +1,18 @@
+package com.bruce.designer.model;
+
+import java.util.Date;
+
+public class TagAlbumBase {
+	
+	private Album album;
+
+	public Album getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(Album album) {
+		this.album = album;
+	}
+	
+	
+}
